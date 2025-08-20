2a87f128a56b244f7b6d76c23850ce7efeb34d1b
Zaz <nhpllm1998+rosenboom@gmail.com>
2025-08-20 09:02:05 -0600

    Merge Strings Alternately - Alt Approach

--- 01 - Easy/merge-strings-alternately.ts
+++ 01 - Easy/merge-strings-alternately.ts
@@ -2,7 +2,7 @@
 // Difficulty: Easy
 // Link: https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75
 
-function mergeAlternately(word1: string, word2: string): string {
+function _mergeAlternately(word1: string, word2: string): string {
   const result: string[] = [];
   const arr1: string[] = Array.from(word1);
   const arr2: string[] = Array.from(word2);
@@ -18,6 +18,26 @@ function mergeAlternately(word1: string, word2: string): string {
   }
 
   return result.join("");
+}
+
+function mergeAlternately(word1: string, word2: string): string {
+  let first = 0,
+    second = 0;
+  let result = "";
+
+  while (first < word1.length || second < word2.length) {
+    if (first < word1.length) {
+      result += word1[first];
+      first++;
+    }
+
+    if (second < word2.length) {
+      result += word2[second];
+      second++;
+    }
+  }
+
+  return result;
 }
 
 // Test cases
